f98b3604375b113906dc3986bfad081e36772b03
sarinalalria <lalrsk01@pfw.edu>
2022-11-27 15:01:16 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-
 const express = require("express");
 const router = express.Router();
-router.use(express.static('../HTML'))
 
+router.use(express.static('../HTML'))
 router.use(express.static('../testVercel1'))
+
 router.get("/index.js", async (req, res, next) => {
   res.set({
     'Access-control-Allow-Origin': '*'
