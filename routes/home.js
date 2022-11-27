c19d07ef8f14d151d122961d6ab7da96e46b9f04
sarinalalria <lalrsk01@pfw.edu>
2022-11-27 17:26:01 -0500

    test

--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+    res.set({
+        'Access-control-Allow-Origin': '*'
+    });
+    return res.redirect('index.html');
 });
 
 module.exports = router;
